470ee6191b718d862bfbfffa1e4fb1e2c7d27096
marcogamer555 <marquitojavier@msn.com>
2024-08-28 20:30:09 -0500

    Update 1 28-08-2024

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,22 +1,30 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:instagram/widgets/button.dart';
 import 'package:instagram/widgets/instagram_logo.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   static String routeName = 'login-screen';
 
-  /// 1. Stateful
-  /// 2. Colocar un botón
-  /// 3. Es crearse una variable _isShowPassword = false
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isShowPassword = false;
+
+  /// 1. Stateful [x]
+  /// 2. Colocar un botón [x]
+  /// 3. Es crearse una variable _isShowPassword = false [x]
   /// 4. Utilizar el setState para cambiar el estado
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:  Padding(
+      body: Padding(
         // EdgeInsets.all(value) -> cuatro esquinas el mismo padding
         // EdgeInsets.only() -> padding en una esquina - nos permite darles diferente valor
         // EdgeInsets.symmetric() -> padding horizontal y vertical
@@ -47,6 +55,7 @@ class LoginScreen extends StatelessWidget {
                 hintText: 'test@test.com',
                 labelText: 'Correo electrónico',
               ),
+
               /// Definir el tipo de teclado
               keyboardType: TextInputType.emailAddress,
             ),
@@ -59,16 +68,118 @@ class LoginScreen extends StatelessWidget {
                 border: const OutlineInputBorder(),
                 labelText: 'Contraseña',
                 // antes
-                // prefix: 
+                // prefix:
                 // sufix -> después
                 // Gesture Detector
                 suffix: GestureDetector(
-                  onTap: () {
-                    /// Colocamos la lógica para mostrar o no mostrar la contraseña
-                  },
-                  child: const Icon(Icons.remove_red_eye)),
+                    onTap: () {
+                      /// Colocamos la lógica para mostrar o no mostrar la contraseña
+                      _isShowPassword = !_isShowPassword;
+                      setState(() {});
+                    },
+                    child: Icon(_isShowPassword
+                        ? Icons.visibility
+                        : Icons.visibility_off)),
               ),
-              obscureText: true,
+              obscureText: _isShowPassword,
+            ),
+
+            /// Text button forgot password
+            /// Align -> alinear widgets con respecto al padre
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Forgot password",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ),
+
+            /// Botón Log in
+            SizedBox(
+              width: double.infinity,
+              height: 44,
+              child: Button(
+                text: 'Log in',
+                onPressed: () {},
+              ),
+            ),
+
+            /// Espacio
+            const SizedBox(height: 20),
+
+            /// Login facebook
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  /// Imagen
+                  /// Image - Widget
+                  Image.asset(
+                    'assets/icons/facebook.png',
+                    height: 17,
+                    width: 17,
+                  ),
+
+                  /// SizedBox
+                  const SizedBox(width: 10),
+
+                  /// Text
+                  const Text(
+                    'Log in with Facebook',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+
+            /// Divider
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                /// Divider
+                /// VerticalDivider
+                SizedBox(
+                  width: 132,
+                  child: Divider(height: 100),
+                ),
+
+                Text(
+                  "OR",
+                  style: TextStyle(color: Colors.grey),
+                ),
+
+                SizedBox(
+                  width: 132,
+                  child: Divider(height: 100),
+                ),
+              ],
+            ),
+
+            /// Don't have accout
+            /// RichText -> varios estilos al texto
+            /// Hola **pepito** como estas
+            /// Text - TextSpan
+            RichText(
+              text: TextSpan(
+                text: "Don't have an account? ",
+                style: const TextStyle(color: Colors.grey),
+                children: [
+                  TextSpan(
+                    text: "Sign Up",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                    recognizer: TapGestureRecognizer()..onTap =(){
+                      print("Redirection");
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
